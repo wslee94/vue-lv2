44554c595bf8506da054ff4de95da08386b88183
wslee94 <leemuo123@gmail.com>
2022-06-26 16:09:18 +0900

    modal component 작성

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,61 +1,81 @@
 <template>
   <div class="inputBox shadow">
-    <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">
+    <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo" />
     <span class="addContainer" v-on:click="addTodo">
       <i class="fas fa-plus addBtn"></i>
     </span>
+
+    <common-modal v-if="showModal">
+      <h3 slot="header">
+        경고!
+        <i class="closeModalBtn fas fa-times" @click="showModal = false"></i>
+      </h3>
+      <div slot="body">아무것도 입력하지 않았습니다.</div>
+      <p slot="footer">copy right</p>
+    </common-modal>
   </div>
 </template>
 
 <script>
+import CommonModal from "./common/CommonModal.vue";
+
 export default {
-  data: function() {
+  data: function () {
     return {
       newTodoItem: "",
-    }
+      showModal: false,
+    };
   },
   methods: {
-    addTodo: function() {
-      if (this.newTodoItem !== '') {
-        this.$emit('addTodoItem', this.newTodoItem)
+    addTodo: function () {
+      if (this.newTodoItem !== "") {
+        this.$emit("addTodoItem", this.newTodoItem);
         this.clearInput();
+      } else {
+        this.showModal = !this.showModal;
       }
     },
-    clearInput: function() {
-      this.newTodoItem='';
-    }
-  }
-}
+    clearInput: function () {
+      this.newTodoItem = "";
+    },
+  },
+  components: {
+    CommonModal,
+  },
+};
 </script>
 
 <style scoped>
-  input:focus {
-    outline: none;
-  }
+input:focus {
+  outline: none;
+}
 
-  .inputBox {
-    background: white;
-    height: 50px;
-    line-height: 50px;
-    border-radius: 5px;
-  }
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
 
-  .inputBox input {
-    border-style: none;
-    font-size: 0.9rem;
-  }
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
 
-  .addContainer {
-    float: right;
-    background: linear-gradient(to right, #6478fb, #8763fb);
-    display: block;
-    width: 3rem;
-    border-radius: 0 5px 5px 0;
-  }
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478fb, #8763fb);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
 
-  .addBtn {
-    color:white;
-    vertical-align: middle;
-  }
+.addBtn {
+  color: white;
+  vertical-align: middle;
+}
 
+.closeModalBtn {
+  color: #42b983;
+}
 </style>
