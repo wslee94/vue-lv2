21bfa7673df9fe5560cc049ca27b0e7d1c82977c
wslee94 <leemuo123@gmail.com>
2022-06-26 17:37:09 +0900

    todo list component transition 적용

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,17 +1,25 @@
 <template>
   <div>
-    <ul>
-      <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem.item" class="shadow" >
-        <i class="checkBtn fas fa-check" 
-          v-bind:class="{ checkBtnCompleted: todoItem.completed }" 
-          v-on:click="toggleComplete(todoItem, index)">
+    <transition-group name="list" tag="ul">
+      <li
+        v-for="(todoItem, index) in todoItems"
+        v-bind:key="todoItem.item"
+        class="shadow"
+      >
+        <i
+          class="checkBtn fas fa-check"
+          v-bind:class="{ checkBtnCompleted: todoItem.completed }"
+          v-on:click="toggleComplete(todoItem, index)"
+        >
         </i>
-        <span v-bind:class="{ textCompleted: todoItem.completed }">{{ todoItem.item }}</span> 
+        <span v-bind:class="{ textCompleted: todoItem.completed }">{{
+          todoItem.item
+        }}</span>
         <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
           <i class="fas fa-trash-alt"></i>
         </span>
       </li>
-    </ul>
+    </transition-group>
   </div>
 </template>
 
@@ -19,52 +27,64 @@
 export default {
   props: ["todoItems"],
   methods: {
-    removeTodo: function(todoItem, index) {
+    removeTodo: function (todoItem, index) {
       this.$emit("removeTodoItem", todoItem, index);
     },
-    toggleComplete: function(todoItem, index) {
-      this.$emit('toggleComplete', todoItem, index);
-    }
+    toggleComplete: function (todoItem, index) {
+      this.$emit("toggleComplete", todoItem, index);
+    },
   },
-}
+};
 </script>
 
 <style scoped>
-  ul {
-    list-style-type: none;
-    padding-left: 0;
-    margin-top: 0;
-    text-align: left;
-  }
+ul {
+  list-style-type: none;
+  padding-left: 0;
+  margin-top: 0;
+  text-align: left;
+}
 
-  li {
-    display: flex;
-    min-height: 50px;
-    height: 50px;
-    line-height: 50px;
-    margin: 0.5rem 0;
-    padding: 0 0.9rem;
-    background: white;
-    border-radius: 5px;
-  }
+li {
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
 
-  .removeBtn {
-    margin-left: auto;
-    color: #de4343;
-  }
+.removeBtn {
+  margin-left: auto;
+  color: #de4343;
+}
 
-  .checkBtn {
-    line-height: 45px;
-    color: #62acde;
-    margin-right: 5px;
-  }
+.checkBtn {
+  line-height: 45px;
+  color: #62acde;
+  margin-right: 5px;
+}
 
-  .checkBtnCompleted {
-    color: #b3adad;
-  }
+.checkBtnCompleted {
+  color: #b3adad;
+}
 
-  .textCompleted {
-    text-decoration: line-through;
-    color: #b3adad
-  }
+.textCompleted {
+  text-decoration: line-through;
+  color: #b3adad;
+}
+
+/* 리스트 아이템 트랜지션 효과 */
+.list-enter-active,
+.list-leave-active {
+  transition: all 1s;
+}
+
+.list-enter,
+.list.leave-to {
+  opacity: 0;
+  transform: translateY(30px);
+}
 </style>
